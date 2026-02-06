05edb6bec8889847e10e72873146522f0f748b43
lazim-mv <lazimlatheef@gmail.com>
2026-02-06 23:00:36 +0530

    clean-up

--- src/app/page.js
+++ src/app/page.js
@@ -4,12 +4,9 @@ import Amenities from "./components/amenities/Amenities";
 import ContactForm from "./components/contact/ContactForm";
 import Container2 from "./components/container2/Container2";
 import Footer from "./components/footer/Footer";
-import Hero from "./components/hero/Hero";
 import HeroTailwind from "./components/hero/HeroTailwind";
-import HeroSlider from "./components/hero2/HeroSlider";
 import Location from "./components/location/Location";
 import VillaPlans from "./components/villa_plans/VillaPlans";
-import VillaPreview from "./components/villapreview/VillaPreview";
 import VillaPreviewTailwind from "./components/villapreview/VillaPreviewTailwind";
 import WhatsAppButton from "./components/WhatsAppButton";
 
